6188d6f667fd94a43d89e2f31fa09a5b5b1ed106
Dwi Siswanto <me@dw1.io>
2023-09-04 01:24:30 +0700

    feat(www): add prePlay function to modified request headers

--- www/assets/script.js
+++ www/assets/script.js
@@ -12,7 +12,39 @@ let c = CodeMirror.fromTextArea(config, style);
 let r = CodeMirror.fromTextArea(request, style);
 r.setSize("49%");
 
+function prePlay() {
+  const firstLine = r.getRange({line: 0, ch: 0}, {line: 1, ch: 0})
+  const lastLine = r.getRange({line: r.lastLine(), ch: 0}, {line: r.lastLine()+1, ch: 0})
+  const cl = lastLine.length
+  const clPattern = /content-length:\s*(\S+)/i;
+  const clHeader = `Content-Length: ${cl}`
+
+  let rVal = r.getValue()
+  if (firstLine.startsWith("POST")) {
+    if (clPattern.test(rVal)) {
+      rVal = rVal.replace(clPattern, (match, oldValue) => {
+        return clHeader
+      })
+      r.setValue(rVal)
+    } else {
+      if (r.getRange({line: r.lastLine()-1, ch: 0}, {line: r.lastLine(), ch: 0}) !== "\n") return;
+      r.replaceRange(`${clHeader}\n\n`,
+        {line: r.lastLine()-1, ch: 0}, {line: r.lastLine(), ch: 0}
+      )
+    }
+
+    return
+  }
+
+  if (firstLine.startsWith("GET")) {
+    const lastVal = r.getRange({line: r.lastLine()-1, ch: 0}, {line: r.lastLine(), ch: 0});
+    if (lastVal !== "\n") r.setValue(`${rVal}\n\n`)
+  }
+}
+
 async function bake(e) {
+  prePlay();
+
   const cfg = c.getValue();
   const req = r.getValue();
   const data = { config: cfg, request: req };
