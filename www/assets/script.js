53fe32b3d4c3b4116fa34b8ed55b791f192eb37b
Dwi Siswanto <me@dw1.io>
2023-09-07 13:01:37 +0700

    feat(www): add introJs

--- www/assets/script.js
+++ www/assets/script.js
@@ -14,10 +14,68 @@ const playBtn = document.getElementById("play");
 const clearBtn = document.getElementById("clear");
 const resetBtn = document.getElementById("reset");
 
+const cm = document.getElementsByClassName("CodeMirror");
+const note = document.getElementById("note");
+
 const l = document.getElementById("logs");
 let c = CodeMirror.fromTextArea(config, style);
 let r = CodeMirror.fromTextArea(request, style);
 r.setSize("49%");
+
+introJs().setOptions({
+  dontShowAgain: true,
+  disableInteraction: true,
+  showProgress: true,
+  showBullets: false,
+  steps: [{
+    title: 'Hello! 👋',
+    intro: 'This web allows you to experiment with your configuration...'
+  },
+  {
+    title: 'Hello! 👋',
+    intro: '...and simulate requests tailored to meet the unique requirements of your app.'
+  },
+  {
+    title: 'Configuration',
+    element: cm[0],
+    intro: 'Feel free to customize the config here as per your preferences!'
+  },
+  {
+    title: 'Request simulation',
+    element: cm[1],
+    intro: 'Also, perform a test of the config by simulating the request you intend to send here.'
+  },
+  {
+    title: 'Play!',
+    element: playBtn,
+    intro: 'Then, click this to execute your request using the configured teler WAF settings.'
+  },
+  {
+    title: 'Response',
+    element: l,
+    intro: 'Once completed, you can view the results here.'
+  },
+  {
+    title: 'Notes',
+    element: note,
+    intro: 'Please consult this note to discover the response details.'
+  },
+  {
+    title: 'Clear logs',
+    element: clearBtn,
+    intro: 'To clean up the logs, simply press this button.'
+  },
+  {
+    title: 'Reset',
+    element: resetBtn,
+    intro: 'If needed, you can reset both the config and request values to their original state.'
+  },
+  {
+    title: 'That\'s it!',
+    element: document.querySelector('.card__image'),
+    intro: 'You\'re all set to go.'
+  }]
+}).start();
 
 function prePlay() {
   const firstLine = r.getRange({line: 0, ch: 0}, {line: 1, ch: 0})
